417c761b8c9adc53335905dfdbfbf3087c5f0dc3
Jaime de Venecia <jaime@augihealth.com>
2026-03-25 16:40:54 -0700

    update(server): improve morgan logging format

--- server/lib/logger.ts
+++ server/lib/logger.ts
@@ -5,8 +5,13 @@ const { combine, timestamp, printf, colorize } = winston.format
 const isProduction = process.env.NODE_ENV === 'production'
 const logLevel = process.env.LOG_LEVEL || (isProduction ? 'info' : 'debug')
 
+const uppercaseLevel = winston.format((info) => {
+  info.level = info.level.toUpperCase()
+  return info
+})
+
 const logFormat = printf(({ level, message, timestamp }) => {
-  return `[${timestamp}] ${level}: ${message}`
+  return `<${timestamp}> [${level}] ${message}`
 })
 
 export const logger = winston.createLogger({
@@ -15,6 +20,7 @@ export const logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
       format: combine(
+        uppercaseLevel(),
         colorize(),
         timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         logFormat
